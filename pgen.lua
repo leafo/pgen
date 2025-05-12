ebe24470fc790fb8ede1745ccbc4e77c440a6eb2
leaf corcoran <leafot@gmail.com>
2025-05-12 14:06:44 -0700

    add dynamic require function

--- pgen.lua
+++ pgen.lua
@@ -98,11 +98,55 @@ end
 -- Compile grammar to C code
 function pgen.compile(grammar, options)
   options = options or {}
-  local output_file = options.output_file or "parser.c"
   local parser_name = options.parser_name or "parser"
 
   local generator = require("pgen.generator")
-  return generator.generate(grammar, parser_name, output_file)
+  return generator.generate(grammar, parser_name)
+end
+
+-- this will require a module that returns a grammar, it will compile it to C
+-- code, then compile it with gcc to temp file, then load the shared object as
+-- a lua module, then return it
+-- this does not cache the compiled code, so it will be recompiled every time require is called
+function pgen.require(module_name)
+  -- Load the grammar module
+  local grammar = require(module_name)
+
+  local parser_name = "parser"
+
+  -- Check if the result is a table (grammar)
+  if type(grammar) ~= "table" then
+    error("Error: Module must return a grammar table")
+  end
+
+  -- Compile the grammar into C code
+  local output, err = pgen.compile(grammar, {
+    -- TODO: generate non-conflicting parser name
+    parser_name = parser_name
+  })
+
+  if not output then
+    error("Error generating parser: " .. (err or "unknown error"))
+  end
+
+  -- Compile the C code to a shared object using gcc, piping to stdin
+  local tmp_so = os.tmpname() .. ".so"
+  local gcc_command = string.format("gcc -shared -o %s -O3 -fPIC -x c - `pkg-config --cflags --libs lua5.1`", tmp_so)
+  local gcc_process = io.popen(gcc_command, "w")
+  if not gcc_process then
+    error("Error: Could not execute gcc for compiling C code")
+  end
+  gcc_process:write(output)
+  gcc_process:close()
+
+  -- Load the compiled shared object
+  local parser = assert(package.loadlib(tmp_so, "luaopen_" .. parser_name))
+
+  -- Cleanup temporary files
+  os.remove(tmp_so)
+
+  -- Return the parser module
+  return parser()
 end
 
 return pgen
